8b0d1e152ecea5c0d4d28e6f6f198ad9b444ac2a
Ed Ignot <edignot@gmail.com>
2020-03-24 18:41:57 -0600

    populate recipes on a page load

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,38 @@
+const body = document.querySelector('body');
+const recipeContainer = document.querySelector('.recipe-container');
+let users = [];
+
+window.addEventListener('load', pageLoad);
+
+function pageLoad() {
+    getRecipe(recipeData);
+    // populate global users array
+    // populateUsers();
+}
+
+function getRecipe(recipeData) {
+    recipeData.map(recipe => {
+        let recipeCard = new Recipes(recipe.id, recipe.image, recipe.ingredients, recipe.intructions, recipe.name, recipe.tags)
+        displayRecipe(recipeCard);
+    })
+}
+
+function displayRecipe(recipeCard) {
+    recipeContainer.insertAdjacentHTML('afterbegin', `
+        <li class="recipe-card">
+          <img src="${recipeCard.image}" class="card-img" alt="recipe picture">
+          <p class="card-title">${recipeCard.name}</p>
+          <div class="card-icons">
+            <ion-icon name="heart-outline" class="card-icon"></ion-icon>
+            <ion-icon name="checkmark-outline" class="card-icon"></ion-icon>
+          </div>
+        </li>
+    `);
+}
+
+
+
+
 //FOR RECIPES
 // getInstructions() {
 //   //returning the instruction array when invoked
