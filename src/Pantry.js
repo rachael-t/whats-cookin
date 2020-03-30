49679d86eb8e70fe73b3b1443b81fac5047da982
Rachael Thomas <rnthomas001@gmail.com>
2020-03-29 19:21:39 -0600

    Update pantry class properties and test

--- src/Pantry.js
+++ src/Pantry.js
@@ -1,17 +1,17 @@
-// if (typeof module !== 'undefined') {
-//   var ingredientInfo = require('../data/ingredients');
-// }
+if (typeof module !== 'undefined') {
+  var ingredientInfo = require('../data/ingredients');
+}
 
 class Pantry {
-  constructor(ingredientInfo, amount) {
-    this.ingredient = ingredientInfo;
-    this.amount = amount;
+  constructor(userPantry) {
+    this.ingredientsStocked = userPantry;
+    //so when we instantiate a user we should instantiate their pantry new Pantry(user.pantry)
   }
   checkIngredients() {
     //invoked under user.cookRecipe() to check if user has enought to cook meal
     //return boolean
     //if false - invoke checkAmountNeeded()
-
+    
   }
   checkAmountNeeded() {
     //loop through user.pantry and user.recipe(?) and return difference of what they have vs needed
--- tests/Pantry-test.js
+++ tests/Pantry-test.js
@@ -8,20 +8,89 @@ const Recipe = require('../src/Recipe');
 
 describe('Pantry', () => {
 
-    let pantry;
-    beforeEach('pantry instance', () => {
-        pantry = new Pantry();
-        recipe = new Recipe();
-    })
+  let pantry;
+  beforeEach('pantry instance', () => {
+    user = new User("Saige O'Kon",
+    1,
+    [
+      {
+        "ingredient": 11477,
+        "amount": 4
+      },
+      {
+        "ingredient": 11297,
+        "amount": 4
+      },
+      {
+        "ingredient": 1082047,
+        "amount": 10
+      }]);
+    pantry = new Pantry(user.pantry);
+    recipe = new Recipe(799732,
+      "google",
+      [{
+          "id": 11477,
+          "quantity": {
+            "amount": 4,
+            "unit": "cup"
+          }
+        },
+        {
+          "id": 11297,
+          "quantity": {
+            "amount": 2,
+            "unit": "cups"
+          }
+        },
+        {
+          "id": 1082047,
+          "quantity": {
+            "amount": 8,
+            "unit": "tsp"
+          }
+        }
+      ],
+      [{
+        "instruction": "Mix & serve.",
+        "number": 1
+      }],
+      "Zucchini Salad",
+      [
+        "salad"
+      ]);
+  })
 
-    it('should be a function', () => {
-        expect(Pantry).to.be.a('function');
-    });
+  it('should be a function', () => {
+    expect(Pantry).to.be.a('function');
+  });
 
-    it('should be an instance of Pantry', () => {
-        expect(pantry).to.be.an.instanceof(Pantry);
-    });
+  it('should be an instance of Pantry', () => {
+    expect(pantry).to.be.an.instanceof(Pantry);
+  });
 
-    it('should be able to store ingredient')
+  it('should be able to store a user\'s list of ingredients', () => {
+    expect(pantry.ingredientsStocked).to.deep.equal([
+      {
+        "ingredient": 11477,
+        "amount": 4
+      },
+      {
+        "ingredient": 11297,
+        "amount": 4
+      },
+      {
+        "ingredient": 1082047,
+        "amount": 10
+      }]);
+  });
+
+  it('should be able to check ingredients in pantry against a recipe', () => {
+    pantry.checkIngredients(recipe)
+    expect(pantry.checkIngredients()).to.equal(false);
+  });
+
+
+
+
 
 })
